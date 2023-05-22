7d4d6a623897d43ef0c7294637fe30044c9dfdf0
Nicolás Heredia Oros <nicolas.heredia.oros@cftmail.cl>
2023-05-22 11:54:24 -0400

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_practica_ultima.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
